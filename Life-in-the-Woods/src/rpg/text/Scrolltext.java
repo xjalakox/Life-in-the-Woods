387dc08621bfc08b24564df37267e4df3df654d9
jalako <sup4tdc@yahoo.de>
2016-04-17 18:42:35 +0200

    asd

---
+++ Life-in-the-Woods/src/rpg/text/Scrolltext.java
@@ -0,0 +1,47 @@
+package rpg.text;
+import java.awt.Color;
+import java.awt.Graphics;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class Scrolltext extends JFrame{
+	
+	
+	public Scrolltext(){
+	    setDefaultCloseOperation(Scrolltext.EXIT_ON_CLOSE);	    
+        
+	    
+	}
+	
+	
+	
+	
+
+    public void paint(Graphics g){
+    	g.clearRect(0, 0, getWidth(), getHeight());
+    	
+		String s = "Hallo, Welt ich bin schwul und heiﬂe Timo und ficke gerne Daumenknicks Lena die auch ein Mann ist";
+		g.setColor(Color.BLUE);
+		char[] c = s.toCharArray();
+		String test = "";
+
+		for(int i = 0; i < c.length; i++){
+			test = test + c[i];
+			g.drawString(test, 10, 100);
+			pause(10);
+		}
+
+			
+    	
+    }
+    
+	public void pause(int test){
+		try {
+			Thread.sleep(test);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
